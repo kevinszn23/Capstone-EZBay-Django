b5a9f2f88abb708a6f3c6280a0301f067d114a29
Kevin Ng <kehfunn@gmail.com>
2022-10-10 11:45:48 -0400

    django setup, views working

---
+++ main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
+# Create your views here.
+class Home(View):
+    def get(self, request):
+        return HttpResponse("eBay Home")
+
+class About(View):
+    def get(self, request):
+        return HttpResponse("eBay Listing")
